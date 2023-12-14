090b366529153f2cfb9a21475b613e7aa59cd03e
EVELYNKINYA <evelynkinya156@gmail.com>
2023-12-14 23:19:59 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 16;
+const num4 = 6;
+const mod = (num3 % num4);
+
+let set = [1, 5, 10, 20, 15];
+let max = Math.max(...set);
